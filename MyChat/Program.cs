972b25eba9265720006195e7f70585cf5a4638fa
Taiyrbek Anarbekov <an.taku2455@gmail.com>
2025-10-23 22:22:06 +0600

    update main code add cache

--- MyChat/Program.cs
+++ MyChat/Program.cs
@@ -1,8 +1,10 @@
+using System.IO.Compression;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Localization;
+using Microsoft.AspNetCore.ResponseCompression;
 using Microsoft.EntityFrameworkCore;
 using MyChat.Models;
 using MyChat.Services;
-using Microsoft.AspNetCore.Localization;
 using System.Globalization;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -13,30 +15,37 @@ builder.Services.AddDbContext<MyChatContext>(opt =>
     opt.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddIdentity<User, IdentityRole<int>>(opt =>
-    {
-        opt.Password.RequiredLength = 6;
-        opt.Password.RequireDigit = true;
-        opt.Password.RequireLowercase = true;
-        opt.Password.RequireUppercase = true;
-        opt.User.RequireUniqueEmail = true;
-    })
-    .AddEntityFrameworkStores<MyChatContext>()
-    .AddErrorDescriber<RuIdentityErrors>()
-    .AddDefaultTokenProviders();
+{
+    opt.Password.RequiredLength = 6;
+    opt.Password.RequireDigit = true;
+    opt.Password.RequireLowercase = true;
+    opt.Password.RequireUppercase = true;
+    opt.User.RequireUniqueEmail = true;
+})
+.AddEntityFrameworkStores<MyChatContext>()
+.AddErrorDescriber<RuIdentityErrors>()
+.AddDefaultTokenProviders();
 
 builder.Services.AddLocalization(options => options.ResourcesPath = "Resources");
 builder.Services.AddControllersWithViews()
     .AddViewLocalization()
     .AddDataAnnotationsLocalization();
 
+builder.Services.AddResponseCompression(options =>
+{
+    options.EnableForHttps = true;
+    options.Providers.Add<BrotliCompressionProvider>();
+    options.Providers.Add<GzipCompressionProvider>();
+});
+builder.Services.Configure<BrotliCompressionProviderOptions>(o => o.Level = CompressionLevel.Optimal);
+builder.Services.Configure<GzipCompressionProviderOptions>(o => o.Level = CompressionLevel.Optimal);
+
+builder.Services.AddResponseCaching();
+builder.Services.AddMemoryCache();
+
 var app = builder.Build();
 
-var supportedCultures = new[]
-{
-    new CultureInfo("en"),
-    new CultureInfo("ru")
-};
-
+var supportedCultures = new[] { new CultureInfo("en"), new CultureInfo("ru") };
 app.UseRequestLocalization(new RequestLocalizationOptions
 {
     DefaultRequestCulture = new RequestCulture("ru"),
@@ -44,23 +53,20 @@ app.UseRequestLocalization(new RequestLocalizationOptions
     SupportedUICultures = supportedCultures
 });
 
-using (var scope = app.Services.CreateScope())
-{
-    var sp = scope.ServiceProvider;
-    await AdminInitializer.Seed(sp);
-}
-
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Home/Error");
-    app.UseHsts();
-}
-
+app.UseResponseCompression();
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 app.UseRouting();
 app.UseAuthentication();
 app.UseAuthorization();
+app.UseResponseCaching();
+
+
+using (var scope = app.Services.CreateScope())
+{
+    var sp = scope.ServiceProvider;
+    await AdminInitializer.Seed(sp);
+}
 
 app.MapControllerRoute(
     name: "default",

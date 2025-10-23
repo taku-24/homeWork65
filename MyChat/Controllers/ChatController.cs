136c2a3655d922f13b5d022a78591042f19eacac
Taiyrbek Anarbekov <an.taku2455@gmail.com>
2025-10-23 22:21:49 +0600

    update controller add cache

--- MyChat/Controllers/ChatController.cs
+++ MyChat/Controllers/ChatController.cs
@@ -1,8 +1,8 @@
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Localization;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Caching.Memory;
 using MyChat.Models;
 
 namespace MyChat.Controllers;
@@ -12,28 +12,37 @@ public class ChatController : Controller
 {
     private readonly MyChatContext _context;
     private readonly UserManager<User> _userManager;
-    private readonly IViewLocalizer _localizer;
+    private readonly IMemoryCache _cache;
 
-    public ChatController(MyChatContext context, UserManager<User> userManager, IViewLocalizer localizer)
+    public ChatController(MyChatContext context, UserManager<User> userManager, IMemoryCache cache)
     {
         _context = context;
         _userManager = userManager;
-        _localizer = localizer;
+        _cache = cache;
     }
     
+    [ResponseCache(Duration = 30, Location = ResponseCacheLocation.Any)]
     [HttpGet]
     public async Task<IActionResult> Index()
     {
         var currentUser = await _userManager.GetUserAsync(User);
         ViewBag.MeId = currentUser!.Id;
-        return View(await _context.Messages
-            .Include(m => m.User)
-            .OrderByDescending(m => m.Id)
-            .Take(30)
-            .OrderBy(m => m.Id)
-            .ToListAsync());
+        
+        if (!_cache.TryGetValue("lastMessages", out List<Message>? lastMessages))
+        {
+            lastMessages = await _context.Messages
+                .Include(m => m.User)
+                .OrderByDescending(m => m.Id)
+                .Take(30)
+                .OrderBy(m => m.Id)
+                .ToListAsync();
+
+            _cache.Set("lastMessages", lastMessages, TimeSpan.FromSeconds(30));
+        }
+
+        return View(lastMessages);
     }
-    
+
     [HttpPost]
     public async Task<IActionResult> Send([FromForm] string text)
     {
@@ -42,11 +51,20 @@ public class ChatController : Controller
             return BadRequest(new { ok = false, message = "Пустое или слишком длинное сообщение" });
 
         var currentUser = await _userManager.GetUserAsync(User);
-        var message = new Message { Text = trimmedText, UserId = currentUser!.Id, SentAt = DateTime.UtcNow };
+        var message = new Message
+        {
+            Text = trimmedText,
+            UserId = currentUser!.Id,
+            SentAt = DateTime.UtcNow
+        };
+
         _context.Add(message);
         await _context.SaveChangesAsync();
+        
+        _cache.Remove("lastMessages");
 
-        return Json(new {
+        return Json(new
+        {
             id = message.Id,
             text = message.Text,
             sentAt = message.SentAt.ToLocalTime().ToString("dd.MM.yyyy HH:mm"),
@@ -55,7 +73,6 @@ public class ChatController : Controller
             avatar = currentUser.AvatarPath
         });
     }
-    
     [HttpGet]
     public async Task<IActionResult> Latest(int afterId = 0)
     {
@@ -63,7 +80,8 @@ public class ChatController : Controller
             .Include(m => m.User)
             .Where(m => m.Id > afterId)
             .OrderBy(m => m.Id)
-            .Select(m => new {
+            .Select(m => new
+            {
                 id = m.Id,
                 text = m.Text,
                 sentAt = m.SentAt.ToLocalTime().ToString("dd.MM.yyyy HH:mm"),
@@ -75,4 +93,5 @@ public class ChatController : Controller
 
         return Json(messages);
     }
+
 }
